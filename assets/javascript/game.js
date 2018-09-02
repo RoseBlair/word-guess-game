4a045d6c97b8a2df0e1e6ace6cbcff4c76496820
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-02 13:53:05 -0400

    added dashes that represent randomized dog breed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 
-var dogType = ["doberman", "blue heeler", "australian shepherd", 
+var dogBreed = ["doberman", "blue heeler", "australian shepherd", 
 "dalmation", "weimaraner", "bichon frise", "komondor", "dachshund", "poodle", "great dane",
 "chihuahua", "golden retreiver", "irish wolfhound", "american pit bull terrier", "rhodesian ridgeback",
 "labrador retreiver", "border collie", "schnauzer", "corgi", "pug", "shiba inu"];
@@ -13,13 +13,23 @@ var letterArray = [];
 var winsCount = document.getElementById("winCount");
 var guessedLetters = document.getElementById("guessedLetters");
 var remainingGuesses = document.getElementById("remainingGuesses");
+var currentWord = document.getElementById("currentWord");
 
 
 // document.getElementByID("remainingGuesses").addEventListener("keyup", function() {
 // };
 
+var randomBreed = dogBreed[Math.floor(Math.random() * dogBreed.length)];
+var breedLength = randomBreed.length;
+currentWord.textContent = "-".repeat(breedLength)
 
 document.onkeyup = function(event) {
+
+
+
+
+
+
     
     var userGuess = event.key;
     letterArray.push(userGuess);
@@ -30,7 +40,8 @@ document.onkeyup = function(event) {
 
 
 
-    // var valueToUse = dogType[Math.floor(Math.random() * dogType.length)];
+
+   
     
 
 
